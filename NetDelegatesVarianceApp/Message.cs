481640635efb93130bff2f1b4f7f1e028deb48c5
maxefimov <efimov.top.academy@gmail.com>
2023-12-19 21:12:39 +0300

    Добавьте файлы проекта.

---
+++ NetDelegatesVarianceApp/Message.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace NetDelegatesVarianceApp
+{
+    class Message
+    {
+        public string? Text { get; set; }
+        public Message(string? text) => Text = text;
+
+        public virtual void Print()
+            => Console.WriteLine($"Message: {Text}");
+    }
+
+    class EmailMessage : Message
+    {
+        public EmailMessage(string? text) : base(text) { }
+
+        public override void Print()
+            => Console.WriteLine($"Email Message: {Text}");
+    }
+
+    class SmsMessage : Message
+    {
+        public SmsMessage(string? text) : base(text) { }
+
+        public override void Print()
+            => Console.WriteLine($"Sms Message: {Text}");
+    }
+}
---
+++ NetDelegatesVarianceApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using NetDelegatesVarianceApp;
+
+
+
+
+// Contravariance
+EmailMessageReceiver emailReceiver = EmailMessanger;
+emailReceiver(new EmailMessage("Hello world"));
+
+emailReceiver = Messanger;
+emailReceiver(new EmailMessage("Hello People"));
+
+
+// For Contravariance
+void EmailMessanger(EmailMessage message)
+{
+    message.Print();
+}
+
+void Messanger(Message message)
+{
+    message.Print();
+}
+
+
+
+
+void CovarianceExample()
+{
+    // Covariance
+    MessageBuilder builder = MessageFactory;
+
+    var mess1 = builder("Hello world");
+    Console.WriteLine(mess1.GetType());
+    mess1.Print();
+
+    builder = EmailMessageFactory;
+    var mess2 = builder("Hello people");
+    Console.WriteLine(mess2.GetType());
+    mess2.Print();
+
+    // For Covariance
+    EmailMessage EmailMessageFactory(string text)
+    {
+        return new EmailMessage(text);
+    }
+
+    Message MessageFactory(string text)
+    {
+        return new Message(text);
+    }
+}
+
+
+// For Contravariance
+delegate void EmailMessageReceiver(EmailMessage message);
+
+// For Covariance
+delegate Message MessageBuilder(string text);
